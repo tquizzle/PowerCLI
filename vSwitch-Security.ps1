e1fcd2c5a396ec8be815b0f5d6eeeafd1338d5c4
Travis Quinnelly <tquinnelly@gmail.com>
2017-09-22 21:17:37 -0500

    Update vSwitch-Security.ps1

--- vSwitch-Security.ps1
+++ vSwitch-Security.ps1
@@ -1,5 +1,8 @@
 <#
-
-
-
+.SYNOPSIS   View your vSwitch Security Options
+.DESCRIPTION  
+.NOTES  This script exports the values to a CSV file so you can sort/filter to view all associated security settings
+.NOTES   with the vSwitches on your ESXi hosts.
 #>
+
+Get-VirtualSwitch -Standard | Select VMHost, Name, @{N="PromiscuousMode";E={if ($_.ExtensionData.Spec.Policy.Security.PromiscuousMode) { "Accept" } Else { "Reject"} }}, @{N="MacChanges";E={if ($_.ExtensionData.Spec.Policy.Security.MacChanges) { "Accept" } Else { "Reject"} }}, @{N="ForgedTransmits";E={if ($_.ExtensionData.Spec.Policy.Security.ForgedTransmits) { "Accept" } Else { "Reject"} }} | Export-Csv ./vSwitch-Security.csv
